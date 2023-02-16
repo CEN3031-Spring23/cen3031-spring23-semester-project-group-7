5f1d25850f56d0c6362fca02f92cfaf655367c5b
Landon Caraway <lmc99@students.uwf.edu>
2023-02-16 10:35:30 -0600

    Added the correct file

--- UserAccount.java
+++ UserAccount.java
@@ -1,4 +1,34 @@
 
-public class UserAccount {
 
-}
+public class UserAccount {
+	
+	string userName;
+	string userNickName;
+	
+	string getUserName() {
+		return userName;
+	}
+	
+	void setUserName(string userName) {
+		this.userName = userName;
+	}
+	
+	string getUserNickName() {
+		return userNickName;
+	}
+
+	void setUserNickName() {
+		this.userNickName = userNickName;
+	}
+	int compareNames(string userName, string userNickName) {
+		if (userName == userNickName) {
+			return -1;
+		}
+		else {
+			return 0;
+		}
+	}
+	void publicInfo() {
+ print.("This is public info");
+	}
+	}
