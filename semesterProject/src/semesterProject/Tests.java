de0a98c2f06dd58f2946e72baa3a165f8fd762d1
Jack Anderson <jack.w.anderson2020@gmail.com>
2023-02-27 20:32:15 -0600

    finished deck tests

--- semesterProject/src/semesterProject/Tests.java
+++ semesterProject/src/semesterProject/Tests.java
@@ -16,20 +16,42 @@ class DeckTests {
 	@Test
 	void testDeckCreation() {
 		int value;
-		value = d1.deck[51].getValue();
+		value = d1.cards[51].getValue();
 		assertEquals(14, value);
 	}
 
 	@Test
 	void testGetCard() {
-
-
-
+		int value;
+		int suit;
+		value = d1.getCard().getValue();
+		suit = d1.getCard().getValue();
+		assertEquals(2, value);
+		assertEquals(1, suit);
 	}
 
 	@Test 
 	void testShuffle() {
+		int value1, value2, value3, value4;
+		int suit1, suit2, suit3, suit4;
 
+		value1 = d1.cards[20].getValue();
+		suit1 = d1.cards[20].getSuit();
+		value2 = d1.cards[40].getValue();
+		suit2 = d1.cards[40].getSuit();
+
+		d1.shuffle();
+
+		value3 = d1.cards[20].getValue();
+		suit3 = d1.cards[20].getSuit();
+		value4 = d1.cards[40].getValue();
+		suit4 = d1.cards[40].getSuit();
+
+		if((value1 == value3) && (suit1 == suit3) && (value2 == value4) && (value2 == value4)) {
+			fail();
+		} else {
+			return;
+		}
 	}
 }
 
