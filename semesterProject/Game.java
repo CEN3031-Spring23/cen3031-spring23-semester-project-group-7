3b698eef7a3c64a42d480afbf79b4f64aef9a7f1
James Gatzmeyer <jrg78@students.uwf.edu>
2023-02-28 18:07:13 -0600

    Finished 'continue?' user story

--- semesterProject/Game.java
+++ semesterProject/Game.java
@@ -1,3 +1,36 @@
 public class Game {
+	
+	public boolean checkBlackjack() {
+		if(player.calculate == 21) 
+			return true;
+		else
+			return false;
+		
+	}
+	
+	public boolean checkBust() {
+		if (player.calcluate() > 21)
+			retrun true;
+		else 
+			return false;
+	}
+	
+	public int checkWinner() {
+		if (player.calculate() > 21)
+			return -1;
+		if (player.calculate() > dealer.calculate())
+			return 1;
+		else if (player.calculate() > dealer.calculate()) 
+			return -1;
+		else if(player.calculate() == dealer.calculate())
+			return 0;
+	}
+ 
+	public boolean playAgain(char continue) {
+	 if (continue == 'n'|| continue == 'N')
+		 return false;
+	 else 
+		 return true;
+ }
     
 }
