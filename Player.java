e67a2b7263041381e70f15ea17b9e53f73f113de
Zane Sampey <zns3@students.uwf.edu>
2023-03-04 15:09:31 -0600

    Card, Dealer, Player modifications and tests working

--- Player.java
+++ Player.java
@@ -2,48 +2,156 @@
 public class Player {
 	private int value;
 	private Card hand[];
-	private int numCards;
+	private int numOfCards;
 	private static final int MAX_VALUE = 11;
 
 	public Player() {
 		value = 0;
 		hand = new Card[MAX_VALUE];
 	}
-
+	
+	public void addCard(Card card) {
+		hand[numOfCards] = card;
+		numOfCards++;
+		value += card.getValue();
+	}
+	
+	public void clearHand() {
+		for (int i = 0; i < numOfCards; i++) {
+			hand[i] = null;
+		}
+		numOfCards = 0;
+		value = 0;
+	}
+	
 	public int getValue() {
 		return value;
 	}
 
-	public void recalculate() {
+	public void recalculateHand() {
 		value = 0;
-		for (int i = 0; i < numCards; i++) {
+		for (int i = 0; i < numOfCards; i++) {
 			value += hand[i].getValue();
 		}
 	}
-
-	public void clear() {
-		for (int i = 0; i < numCards; i++) {
-			hand[i] = null;
-		}
-		numCards = 0;
-		value = 0;
-	}
-
-	public void addCard(Card card) {
-		hand[numCards] = card;
-		numCards++;
-		value += hand[numCards].getValue();
-	}
-
+	
 	public void aceBuster() {
-		for (int i = 0; i < MAX_VALUE; i++) {
+		for (int i = 0; i < numOfCards; i++) {
 			if (hand[i].getRank() == 1) {
 				if (value > 21) {
 					hand[i].setValue(1);
-					recalculate();
+					recalculateHand();
 				}
 			}
 		}
+	}
+	
+	public String getHand() {
+		String outputHand = "Player Hand: ";
+		for(int i = 0; i < numOfCards; i++) {
+			if(i == numOfCards - 1) {
+				if(hand[i].getSuit() == 0) {
+					if(hand[i].getRank() == 1) {
+						outputHand += "Ace of Clubs";
+					} else if(hand[i].getRank() == 11) {
+						outputHand += "Jack of Clubs";
+					} else if(hand[i].getRank() == 12) {
+						outputHand +=  "Queen of Clubs";
+					} else if(hand[i].getRank() == 13) {
+						outputHand +=  "King of Clubs";
+					} else {
+						outputHand += hand[i].getRank() + " of Clubs";
+					}
+				} else if(hand[i].getSuit() == 1){
+					if(hand[i].getRank() == 1) {
+						outputHand += "Ace of Spades";
+					} else if(hand[i].getRank() == 11) {
+						outputHand += "Jack of Spades";
+					} else if(hand[i].getRank() == 12) {
+						outputHand +=  "Queen of Spades";
+					} else if(hand[i].getRank() == 13) {
+						outputHand +=  "King of Spades";
+					} else {
+						outputHand += hand[i].getRank() + " of Spades";
+					}
+				} else if(hand[i].getSuit() == 2){
+					if(hand[i].getRank() == 1) {
+						outputHand += "Ace of Diamonds";
+					} else if(hand[i].getRank() == 11) {
+						outputHand += "Jack of Diamonds";
+					} else if(hand[i].getRank() == 12) {
+						outputHand +=  "Queen of Diamonds";
+					} else if(hand[i].getRank() == 13) {
+						outputHand +=  "King of Diamonds";
+					} else {
+						outputHand += hand[i].getRank() + " of Diamonds";
+					}
+				} else if(hand[i].getSuit() == 3){
+					if(hand[i].getRank() == 1) {
+						outputHand += "Ace of Hearts";
+					} else if(hand[i].getRank() == 11) {
+						outputHand += "Jack of Hearts";
+					} else if(hand[i].getRank() == 12) {
+						outputHand +=  "Queen of Hearts";
+					} else if(hand[i].getRank() == 13) {
+						outputHand +=  "King of Hearts";
+					} else {
+						outputHand += hand[i].getRank() + " of Hearts";
+					}
+			    }
+			} else {
+				if(hand[i].getSuit() == 0) {
+					if(hand[i].getRank() == 1) {
+						outputHand += "Ace of Clubs, ";
+					} else if(hand[i].getRank() == 11) {
+					outputHand += "Jack of Clubs, ";
+					} else if(hand[i].getRank() == 12) {
+					outputHand +=  "Queen of Clubs, ";
+					} else if(hand[i].getRank() == 13) {
+					outputHand +=  "King of Clubs, ";
+					} else {
+					outputHand += hand[i].getRank() + " of Clubs, ";
+					}
+				} else if(hand[i].getSuit() == 1){
+					if(hand[i].getRank() == 1) {
+						outputHand += "Ace of Spades, ";
+					} else if(hand[i].getRank() == 11) {
+						outputHand += "Jack of Spades, ";
+					} else if(hand[i].getRank() == 12) {
+						outputHand +=  "Queen of Spades, ";
+					} else if(hand[i].getRank() == 13) {
+						outputHand +=  "King of Spades, ";
+					} else {
+						outputHand += hand[i].getRank() + " of Spades, ";
+					}
+				} else if(hand[i].getSuit() == 2){
+					if(hand[i].getRank() == 1) {
+						outputHand += "Ace of Diamonds, ";
+					} else if(hand[i].getRank() == 11) {
+						outputHand += "Jack of Diamonds, ";
+					} else if(hand[i].getRank() == 12) {
+						outputHand +=  "Queen of Diamonds, ";
+					} else if(hand[i].getRank() == 13) {
+						outputHand +=  "King of Diamonds, ";
+					} else {
+						outputHand += hand[i].getRank() + " of Diamonds, ";
+					}
+				} else if(hand[i].getSuit() == 3){
+					if(hand[i].getRank() == 1) {
+						outputHand += "Ace of Hearts, ";
+					} else if(hand[i].getRank() == 11) {
+						outputHand += "Jack of Hearts, ";
+					} else if(hand[i].getRank() == 12) {
+						outputHand +=  "Queen of Hearts, ";
+					} else if(hand[i].getRank() == 13) {
+						outputHand +=  "King of Hearts, ";
+					} else {
+						outputHand += hand[i].getRank() + " of Hearts, ";
+					}
+				}
+			}
+		}
+		return outputHand;
 	}
 
 }
--- Tests.java
+++ Tests.java
@@ -14,7 +14,7 @@ class DeckTests {
 	@Test
 	void testDeckCreation() {
 		int value;
-		value = d1.cards.get(51).getValue();
+		value = d1.cards[51].getValue();
 		assertEquals(14, value);
 	}
 
@@ -33,17 +33,17 @@ class DeckTests {
 		int rank1, rank2, rank3, rank4;
 		int suit1, suit2, suit3, suit4;
 
-		rank1 = d1.cards.get(20).getRank();
-		suit1 = d1.cards.get(20).getSuit();
-		rank2 = d1.cards.get(40).getRank();
-		suit2 = d1.cards.get(40).getSuit();
+		rank1 = d1.cards[20].getRank();
+		suit1 = d1.cards[20].getSuit();
+		rank2 = d1.cards[40].getRank();
+		suit2 = d1.cards[40].getSuit();
 
 		d1.shuffle();
 
-		rank3 = d1.cards.get(20).getRank();
-		suit3 = d1.cards.get(20).getSuit();
-		rank4 = d1.cards.get(40).getRank();
-		suit4 = d1.cards.get(40).getSuit();
+		rank3 = d1.cards[20].getRank();
+		suit3 = d1.cards[20].getSuit();
+		rank4 = d1.cards[40].getRank();
+		suit4 = d1.cards[40].getSuit();
 
 		if((rank1 == rank3) && (suit1 == suit3) && (rank2 == rank4) && (rank2 == rank4)) {
 			fail();
@@ -52,10 +52,9 @@ class DeckTests {
 	}
 }
 
-
 class CardTests {
 
-	Card cards = new Card(3, 10, 12); //Order is Suit, Value, Rank
+	Card cards = new Card(3, 10, 12);
 
     @Test
     public void suitTest() {
@@ -111,40 +110,19 @@ class GameTests {
 
 }
 
+
 class DealerJUnitTester {
 
 	@Test
 	void createDealerTest() {
 		Dealer dealer = new Dealer();
 
-		assertEquals(0, dealer.calculateHand());
-		assertEquals("", dealer.getHand());
+		assertEquals(0, dealer.getValue());
+		assertEquals("Dealer Hand: ", dealer.getHand());
 	}
-
+	
 	@Test
-	void aceValueTest() {
-		Dealer dealer = new Dealer();
-
-		assertEquals(11, dealer.getAceValue());
-
-		dealer.setAce(1);
-		assertEquals(1, dealer.getAceValue());
-	}
-
-	@Test
-	void dealerHandTest() {
-		Dealer dealer = new Dealer();
-		Card card1 = new Card(1, 2, 2);
-		Card card2 = new Card(3, 10, 12);
-
-		dealer.addCard(card1);
-		assertEquals("Card 1: 1, 2, 2\n", dealer.getHand());
-		dealer.addCard(card2);
-		assertEquals("Card 1: 1, 2, 2\nCard 2: 3, 10, 12\n", dealer.getHand());
-	}
-
-	@Test
-	void calculateDealerHandTest() {
+	void addCardDealerTest() {
 		Dealer dealer = new Dealer();
 		Card card1 = new Card(1, 2, 2);
 		Card card2 = new Card(3, 10, 12);
@@ -152,7 +130,19 @@ class DealerJUnitTester {
 		dealer.addCard(card1);
 		dealer.addCard(card2);
 
-		assertEquals(12, dealer.calculateHand());
+		assertEquals(12, dealer.getValue());
+	}
+	
+	@Test
+	void getDealerHandTest() {
+		Dealer dealer = new Dealer();
+		Card card1 = new Card(1, 2, 2);
+		Card card2 = new Card(3, 10, 12);
+
+		dealer.addCard(card1);
+		assertEquals("Dealer Hand: 2 of Spades", dealer.getHand());
+		dealer.addCard(card2);
+		assertEquals("Dealer Hand: 2 of Spades, Queen of Hearts", dealer.getHand());
 	}
 
 	@Test
@@ -166,8 +156,21 @@ class DealerJUnitTester {
 		dealer.addCard(card2);
 		dealer.clearHand();
 
-		assertEquals("", dealer.getHand());
+		assertEquals("Dealer Hand: ", dealer.getHand());
 
+	}
+	
+	@Test
+	void aceBusterDealerTest() {
+		Dealer dealer = new Dealer();
+		Card card1 = new Card(1, 11, 1);
+		Card card2 = new Card(2, 11, 1);
+		
+		dealer.addCard(card1);
+		dealer.addCard(card2);
+		dealer.aceBuster();
+		
+		assertEquals(12, dealer.getValue());
 	}
 
 }
@@ -175,39 +178,62 @@ class DealerJUnitTester {
 class PlayerJUnitTest {
 
 	@Test
-	void chooseAceTest() {
-		Player player = new player();
-		Card card1 = new Card(1, 2, 12);
-		Card card2 = new Card(1, 8, 12);
+	void createPlayerTest() {
+		Player player = new Player();
+
+		assertEquals(0, player.getValue());
+	}
+
+	@Test
+	void addCardPlayerTest() {
+		Player player = new Player();
+		Card card1 = new Card(1, 2, 2);
+		Card card2 = new Card(3, 10, 12);
 
 		player.addCard(card1);
 		player.addCard(card2);
 
-		assertEquals(1, player.chooseAce());
+		assertEquals(12, player.getValue());
 	}
-
+	
 	@Test
-	void calculateTest() {
+	void getPlayerHandTest() {
 		Player player = new Player();
-		Card card1 = new Card(1, 2, 12);
-		Card card2 = new Card(1, 8, 12);
+		Card card1 = new Card(1, 2, 2);
+		Card card2 = new Card(3, 10, 12);
 
 		player.addCard(card1);
-		player.addCArd(card2);
-
-		assertEquals(10, player.calculate());
+		assertEquals("Player Hand: 2 of Spades", player.getHand());
+		player.addCard(card2);
+		assertEquals("Player Hand: 2 of Spades, Queen of Hearts", player.getHand());
 	}
 
 	@Test
-	void clearTest() {
+	void clearPlayerHandTest() {
 		Player player = new Player();
-		Card card = new Card()1, 2, 12;
+		Card card1 = new Card(1, 2, 2);
+		Card card2 = new Card(3, 10, 12);
 
-		player.addCard(card);
-		player.clear();
 
-		assertEquals(0, value);
-		assertEquals(0, numCards);
+		player.addCard(card1);
+		player.addCard(card2);
+		player.clearHand();
+
+		assertEquals("Player Hand: ", player.getHand());
+
+	}
+	
+	@Test
+	void aceBusterPlayerTest() {
+		Player player = new Player();
+		Card card1 = new Card(1, 11, 1);
+		Card card2 = new Card(2, 11, 1);
+		
+		player.addCard(card1);
+		player.addCard(card2);
+		player.aceBuster();
+		
+		assertEquals(12, player.getValue());
 	}
 
 }
