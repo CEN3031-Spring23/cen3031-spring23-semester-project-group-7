e26aa51eefd4b90a65aa04be95c18ad620c54503
Zane Sampey <zns3@students.uwf.edu>
2023-04-30 22:26:32 -0500

    Updated DealerJUnitTests

--- tests/DealerJUnitTests.java
+++ tests/DealerJUnitTests.java
@@ -9,7 +9,6 @@ class DealerJUnitTests {
 		Dealer dealer = new Dealer();
 
 		assertEquals(0, dealer.getValue());
-		assertEquals("Dealer Hand: ", dealer.getHand());
 	}
 	
 	@Test
@@ -23,18 +22,6 @@ class DealerJUnitTests {
 
 		assertEquals(12, dealer.getValue());
 	}
-	
-	@Test
-	void getDealerHandTest() {
-		Dealer dealer = new Dealer();
-		Card card1 = new Card(1, 2, 2);
-		Card card2 = new Card(3, 10, 12);
-
-		dealer.addCard(card1);
-		assertEquals("Dealer Hand: 2 of Spades", dealer.getHand());
-		dealer.addCard(card2);
-		assertEquals("Dealer Hand: 2 of Spades, Queen of Hearts", dealer.getHand());
-	}
 
 	@Test
 	void clearDealerHandTest() {
@@ -47,7 +34,7 @@ class DealerJUnitTests {
 		dealer.addCard(card2);
 		dealer.clearHand();
 
-		assertEquals("Dealer Hand: ", dealer.getHand());
+		assertEquals(0, dealer.getValue());
 
 	}
 	
